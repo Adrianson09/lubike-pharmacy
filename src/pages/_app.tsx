0b1107055d282102b5ca47d7acfccf6bcdcc8ccb
Adrian Guerrero <adrianson@outlook.com>
2023-03-22 22:02:57 -0600

    add theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import '@/styles/globals.css'
-import { ThemeProvider } from '@mui/material'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
 import { lightTheme } from '../../themes/light-theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={lightTheme}>
+        <CssBaseline/>
         <Component {...pageProps}/>
     </ThemeProvider>
   )
