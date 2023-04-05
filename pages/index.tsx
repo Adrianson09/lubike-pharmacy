c6ee40cade62f9b7cf46235ff626d1a630afb3d6
Adrian Guerrero <adrianson@outlook.com>
2023-04-04 20:13:53 -0600

    add products Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,23 @@
 import type {NextPage} from 'next';
-import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 
 import { ShopLayout } from "../Components/layouts";
 import { initialData } from '../database/products';
+import { ProductList } from '../Components/products';
 
 
 
-export default function Home() {
+ const Home: NextPage =( ) => {
   return (
     <ShopLayout title={"Bike Pharmacy - Home"} pageDescription={"Bike shop tienda y ciclo para bicicletas en Costa Rica"}>
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2' sx={{mb: 1}} >Todos los Productos</Typography>
 
-      <Grid container spacing={4}>
-          {
-            initialData.products.map( product =>(
-              <Grid item xs={6} sm={ 4 } key={product.slug}>
-                <Card>
-                  <CardActionArea>
-                    <CardMedia
-                    component='img'
-                    image={`products/${ product.images[0]}`}
-                    alt={product.title}
-                    />
-                  </CardActionArea>
-                </Card>
-              </Grid>
-            ))
-          }
-      </Grid>
-
-
+      <ProductList
+      products={ initialData.products as any}
+      />
     </ShopLayout>
   )
 }
+
+export default Home
